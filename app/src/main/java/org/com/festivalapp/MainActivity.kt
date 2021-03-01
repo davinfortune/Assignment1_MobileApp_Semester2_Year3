7bcc555a702ee7e707f7f5a405aa85c6bdcc15e2
davinfortune <davinfortune@gmail.com>
2021-03-01 20:55:00 +0000

    My First commit

---
+++ app/src/main/java/org/com/festivalapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.com.festivalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}

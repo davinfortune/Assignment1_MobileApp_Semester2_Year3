dd357e84989754c29b94fa73869373ee11209075
davinfortune <davinfortune@gmail.com>
2021-03-07 16:03:52 +0000

    Menu Fragment System working

--- app/src/main/java/org/com/festivalapp/MainActivity.kt
+++ app/src/main/java/org/com/festivalapp/MainActivity.kt
@@ -1,12 +1,42 @@
 package org.com.festivalapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.com.festivalapp.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        openFragment(HomeFragment())
+        bottomNav.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    openFragment(HomeFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.gallery -> {
+                    openFragment(GalleryFragment())
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.tickets -> {
+                    openFragment(Ticket())
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
+    }
 
+    fun openFragment(fragment: Fragment) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 }
